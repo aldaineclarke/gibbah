c41804b4944affd5e4ccf8099cc42907653eeaa7
aldaine-AG <aldaineclarke1@gmail.com>
2022-04-12 00:14:09 -0500

    Created the directive to observe and animate the attached element

---
+++ src/app/Directives/observed.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ObservedDirective } from './observed.directive';
+
+describe('ObservedDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ObservedDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/Directives/observed.directive.ts
@@ -0,0 +1,36 @@
+import { Directive, ElementRef, EventEmitter, Output } from '@angular/core';
+
+@Directive({
+  selector: '[observe]'
+})
+export class ObservedDirective {
+
+  @Output() inView : EventEmitter<Element> = new EventEmitter();
+
+  private _observer:IntersectionObserver | undefined;
+  private options = {
+    // threshold: 0,
+    rootMargin: "-100px"
+  }
+  constructor(private _element: ElementRef<any>) { 
+    this.createAndObserver(this._element.nativeElement);
+  }
+
+  createAndObserver(element:HTMLElement){
+      this._observer = new IntersectionObserver((entries, observer)=>{
+        entries.forEach((entry)=>{
+          if(entry.isIntersecting){
+            if(!entry.target.classList.contains("active")){
+              entry.target.classList.add("active");
+            }
+            entry.target.classList.add("is-Inview");
+            this.inView.emit(entry.target);
+          } 
+        })
+      }, this.options);
+
+      this._observer.observe(element);
+  }
+
+
+}
